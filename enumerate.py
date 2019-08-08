548d617d55f722a2fbb91582f930e1b6bb1bc2af
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2019-08-08 16:59:40 +0100

    2nd commit

--- enumerate.py
+++ enumerate.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
 
+import hid
+
+from d in hid.enumerate():
+keys = list(d.keys())
+keys.sort()
+for keys in keys:
+    print("%s : %s" % (key, d[key]))
