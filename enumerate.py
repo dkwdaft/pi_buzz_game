57ac16a296669bd6e2fa710efaa01e03b4cfd3a8
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2019-08-09 10:02:54 +0100

    2nd commit

--- enumerate.py
+++ enumerate.py
@@ -3,8 +3,8 @@ from __future__ import print_function
 
 import hid
 
-from d in hid.enumerate():
-keys = list(d.keys())
-keys.sort()
-for keys in keys:
-    print("%s : %s" % (key, d[key]))
+for d in hid.enumerate():
+    keys = list(d.keys())
+    keys.sort()
+    for key in keys:
+        print("%s : %s" % (key, d[key]))
