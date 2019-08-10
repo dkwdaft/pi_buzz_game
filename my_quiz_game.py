d7a3583e01ef1061f673bd60e8376af67f5415db
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2019-08-10 16:14:53 +0100

    2nd commit

--- my_quiz_game.py
+++ my_quiz_game.py
@@ -1,6 +1,27 @@
 from __future__ import print_function
 import BuzzController
 import time
-import _thread
 import thread
 from random import shuffle
+
+import_questions = [
+    {"question": "what Is the capital of Australia", "Answers": ["Canberra", "Sydney", "Hobart", "Melbourne"]},
+    {"question": "What is the capital of Japan", "Answers": ["Tokyo", "Hiroshima", "Osaka", "Kyoto"]},
+]
+questions = []
+score = [0, 0, 0, 0]
+for question in import_questions:
+    buttons = ["blue", "orange", "green", "yellow"]
+    new_answer = {}
+    shuffle(buttons)
+    new_answer['question'] = question['question']
+    for i in range(4):
+        if i == 0:
+            new_answer["correct"] = buttons[i]
+            new_answer[buttons[i]] = question["answers"][i]
+            questions.append(new_answer)
+
+            buzz = BuzzController.BuzzController()
+            for question in questions:
+                question_answered = False
+                available_answers = ["Blue", "Orange"]
