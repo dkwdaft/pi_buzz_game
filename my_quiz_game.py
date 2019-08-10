a188e7b0d145f10fd233acf08df01087e35406d7
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2019-08-10 16:46:16 +0100

    2nd commit

--- my_quiz_game.py
+++ my_quiz_game.py
@@ -24,4 +24,35 @@ for question in import_questions:
             buzz = BuzzController.BuzzController()
             for question in questions:
                 question_answered = False
-                available_answers = ["Blue", "Orange"]
+                available_answers = ["Blue", "Orange", "Green", "Yellow"]
+                available_controllers = [0, 1, 2, 3]
+
+                while not question_answered:
+                    print(question["question"])
+
+                    for i in available_answers:
+                        print(i + " " + question[i.lower()])
+
+                    thread.start_new_thread(buzz.light_blink, (available_controllers,))
+                    controller = buzz.controller_get_first_pressed("red", available_controllers)
+                    buzz.light_blinking = False
+                    buzz.light_set(controller, True)
+                    time.sleep(0.5)
+
+                    while True:
+                        button = buzz.get_button_pressed(controller)
+                        if button and button != "red":
+                            if button == question["correct"]:
+                                print("Controller " + str(controller) + " was correct")
+                                question_answered = True
+                                score[controller] += 1
+                                break
+                            elif button.capitalize() in available_answers:
+                                print("Sorry incorrect answer")
+                            available_controllers.remove(controller)
+                            available_answers.remove(button.capitalize())
+                            break
+                    buzz.light_set(controller, False)
+                    time.sleep(1)
+
+                    print("Final score:" + score)
